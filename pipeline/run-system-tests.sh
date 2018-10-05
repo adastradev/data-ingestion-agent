b80fc6a89382fa0e3f3843e77906f04c5234caeb
Cory Routledge <croutledge@aais.com>
2018-10-05 08:33:26 -0500

    First go at getting a correctly formatted ssh key written in the pipeline

--- pipeline/run-system-tests.sh
+++ pipeline/run-system-tests.sh
@@ -1,8 +1,13 @@
+# Configure our test SSH key
+echo "AUTOMATED_TEST_SSH_KEY" > test/system/automated-system-tests.pem
+# Make sure the key permissions are not 'too open'
+chmod 600 test/system/automated-system-tests.pem
+sed -i -e "s|AUTOMATED_TEST_SSH_KEY|$AUTOMATED_TEST_SSH_KEY|" test/system/automated-system-tests.pem
+
+# Configure system test
 sed -e "s|TEST_INSTANCE_SSH_KEY_NAME|$TEST_INSTANCE_SSH_KEY_NAME|" test/system/instanceConfig.json.sample > test/system/instanceConfig.json
 sed -i "s|TEST_INSTANCE_SECURITYGROUP|$TEST_INSTANCE_SECURITYGROUP|" test/system/instanceConfig.json
 sed -i "s|TEST_INSTANCE_SUBNETID|$TEST_INSTANCE_SUBNETID|" test/system/instanceConfig.json
-
-export NORMALIZED_DOCKER_TAG=`echo $BITBUCKET_BRANCH | sed -e "s|\/|_|"`
 
 npm install
 node_modules/typescript/bin/tsc
