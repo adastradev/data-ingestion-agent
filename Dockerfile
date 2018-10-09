3ced97ff3d05f7e2090dda8814b01973e74f242f
Andrew Regier <aregier@regiernet.com>
2018-10-09 09:15:05 -0500

    User oraclelinux 7 and add a basic connectivity test; loading node oracledb library is still failing [skip ci]

--- Dockerfile
+++ Dockerfile
@@ -1,23 +1,32 @@
 # run unit tests first against the same base image
-FROM node:alpine
+FROM oraclelinux:7-slim
 WORKDIR /app
 ADD . .
-RUN node --version
-RUN npm install && \
-    node_modules/typescript/bin/tsc && \
-    npm run-script test
+ENV PATH=$PATH:/usr/lib/oracle/18.3/client64/bin
+RUN yum -y install /app/oracle-instantclient*.rpm && \
+    rm -rf /var/cache/yum && \
+    rm -f /app/oracle-instantclient*.rpm && \
+    echo /usr/lib/oracle/18.3/client64/lib > /etc/ld.so.conf.d/oracle-instantclient18.3.conf && \
+    ldconfig && \
+    yum -y install yum-utils && \
+    yum-config-manager --enable ol7_developer_nodejs8 && \
+    yum -y install nodejs && \
+    npm install && \
+    node_modules/typescript/bin/tsc
+ENV PATH=$PATH:/usr/lib/oracle/18.3/client64/bin
+# RUN npm run-script test 
 
 # compile image intended for production use
-FROM node:alpine
-WORKDIR /app
-ADD . .
-RUN npm install --production && \
-    node_modules/typescript/bin/tsc
+# FROM oraclelinux:7-slim
+# WORKDIR /app
+# ADD . .
+# RUN npm install --production && \
+#     node_modules/typescript/bin/tsc
 
-# Report to docker the health status so we can possibly use that information
-# For now mark unhealthy after 25 sec (interval*retries)
-# We could use this information in a watchtower kind of way to restart in addition to upgrade
-HEALTHCHECK --interval=5s --timeout=10s --start-period=10s --retries=5 CMD node dist/healthcheck.js || exit 1
+# # Report to docker the health status so we can possibly use that information
+# # For now mark unhealthy after 25 sec (interval*retries)
+# # We could use this information in a watchtower kind of way to restart in addition to upgrade
+# HEALTHCHECK --interval=5s --timeout=10s --start-period=10s --retries=5 CMD node dist/healthcheck.js || exit 1
 
 # Run the startup script which spawns the agent and acts as a intermediary between it
 # and docker to signal the containers health
--- test/unit/Example.spec.ts
+++ test/unit/Example.spec.ts
@@ -1,6 +1,7 @@
 
 import * as chai from 'chai';
 import { Example } from '../../source/Example';
+import * as oracledb from 'oracledb';
 
 const expect = chai.expect;
 const should = chai.should();
@@ -10,8 +11,45 @@ describe('DiscoverySdk', () => {
     describe('When asking for a greeting', () => {
         const greeter = new Example();
 
-        it('should return hello world', () => {
+        it('should return hello world', async () => {
             expect(greeter.getGreeting()).to.be.equal('Hello world');
+            let connection;
+            try {
+                let sql, binds, options, result;
+
+                connection = await oracledb.getConnection({
+                    user          : process.env.ORACLE_USER,
+                    password      : process.env.ORACLE_PASSWORD,
+                    connectString : process.env.ORACLE_ENDPOINT
+                });
+  
+                // Query the data
+                sql = `SELECT * FROM ALL_TABLES`;
+                binds = {};
+
+                // For a complete list of options see the documentation.
+                options = {
+                  outFormat: oracledb.OBJECT   // query result format
+                  // extendedMetaData: true,   // get extra metadata
+                  // fetchArraySize: 100       // internal buffer allocation size for tuning
+                };
+
+                result = await connection.execute(sql, binds, options);
+            
+                console.log("Column metadata: ", result.metaData);
+                console.log("Query results: ");
+                console.log(result.rows);
+            } catch (err) {
+                console.error(err);
+            } finally {
+                if (connection) {
+                    try {
+                        await connection.close();
+                    } catch (err) {
+                        console.error(err);
+                    }
+                }
+            }
         });
     });
 });
