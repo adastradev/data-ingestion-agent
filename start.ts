8cb2d9dfd4c7ce75de05488cf64f072606937d30
Lindsey Brown <lbrown@aais.com>
2018-10-09 14:16:05 -0500

    Updates to DIA ASTRA-7314 Resiliency

--- start.ts
+++ start.ts
@@ -4,27 +4,45 @@ import * as shell from 'child_process';
 // optional args
 var args = process.argv.slice(2);
 var target = ['dist/index.js'].concat(args);
+var restartCounter = 0;
+var child;
 
-// start agent process
-var child = shell.spawn('node', target);
-child.unref(); // apparently detangles event loop of the child from the parent
-child.stdout.on('data', function(data) {
-    console.log(data.toString());
-});
-child.stderr.on('data', function(data) {
-    console.log(data.toString());
-});
 
-var childState = {status: 'running'};
-child.on('exit', (code, signal) => {
-    childState.status = 'exited';
+var onExitHandler = (code, signal) =>{
+    restartCounter++;
+    console.log('restarting container');
     
+    if(restartCounter < 5){
+       child = spawnAgent();
+    }else{
+        childState.status = 'exited';
+    }
+
     if (args[0] === "preview") {
         if (server) {
             server.close();
         }
     }
+}
+
+
+var spawnAgent = () =>
+{
+// start agent process
+var child = shell.spawn('node', target);
+child.unref(); // apparently detangles event loop of the child from the parent
+child.stdout.on('data', function (data) {
+    console.log(data.toString());
 });
+child.on('exit', onExitHandler);
+child.stderr.on('data', function (data) {
+    console.log(data.toString());
+});
+return child;
+};
+
+child = spawnAgent();
+var childState = { status: 'running' };
 
 // start server
 const app = express();
@@ -39,8 +57,9 @@ app.get('/', (req, res) => {
 
     res.send(childState.status);
 });
+
 var server = app.listen(port, () => console.log('healthcheck server'));
-process.on('SIGTERM', function() {
+process.on('SIGTERM', function () {
     child.kill("SIGTERM");
     server.close();
 });
