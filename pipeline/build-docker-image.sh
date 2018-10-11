736a4561e2944a02f39821874b5e73ed7fad579c
Andrew Regier <aregier@regiernet.com>
2018-10-11 09:09:36 -0500

    fix build pipeline to use shell script to inject in ARGS for dockerfile

--- pipeline/build-docker-image.sh
+++ pipeline/build-docker-image.sh
@@ -4,7 +4,7 @@ docker build \
 --build-arg ORACLE_USER=$ORACLE_USER \
 --build-arg ORACLE_PASSWORD=$ORACLE_PASSWORD \
 --build-arg TEST_TARGET=integration-test \
--t data-ingestion-agent:latest .
+-t $IMAGE_NAME .
 
 docker tag data-ingestion-agent:latest adastradev/data-ingestion-agent:latest
 docker push adastradev/data-ingestion-agent:latest
