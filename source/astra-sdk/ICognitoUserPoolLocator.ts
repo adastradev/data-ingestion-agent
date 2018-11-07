01deca33a4c4a5fcc23fed84a807e6f4e2383121
Andrew Regier <aregier@regiernet.com>
2018-11-07 15:55:17 -0600

    WIP; remove code being extracted to user-management-sdk-js

--- source/astra-sdk/ICognitoUserPoolLocator.ts
+++
@@ -1,5 +0,0 @@
-import { ICognitoUserPoolApiModel } from './ICognitoUserPoolApiModel';
-
-export interface ICognitoUserPoolLocator {
-    getPoolForUsername(userName: string): Promise<ICognitoUserPoolApiModel>;
-}
