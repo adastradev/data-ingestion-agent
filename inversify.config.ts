0118baabee6576e081978bb6afe2dccc150f3188
Andrew Regier <aregier@regiernet.com>
2018-11-16 14:33:53 -0600

    Log additional information for errors from axios; Fix undefined S3 bucket path in data ingestion agent

--- inversify.config.ts
+++ inversify.config.ts
@@ -81,107 +81,109 @@ const poolLocator = new CognitoUserPoolLocatorUserManagement(region);
 const authManager = new AuthManager(poolLocator, region);
 
 const startup = async () => {
-    if (logger.level === 'silly') { // truly silly debugging for testing proxy operation
-        logger.silly('test GET http://example.com (via axios)');
-        const exampleResponse = await axios.get('http://example.com');
-        if (exampleResponse.status === 200) {
-            logger.silly('SUCCESS: GET http://example.com');
-        }
+    try {
+        if (logger.level === 'silly') { // truly silly debugging for testing proxy operation
+            logger.silly('test GET http://example.com (via axios)');
+            const exampleResponse = await axios.get('http://example.com');
+            if (exampleResponse.status === 200) {
+                logger.silly('SUCCESS: GET http://example.com');
+            }
 
-        logger.silly('test GET https://example.com (via axios)');
-        const exampleHttpsResponse = await axios.get('https://example.com');
-        if (exampleHttpsResponse.status === 200) {
-            logger.silly('SUCCESS: GET https://example.com');
-        }
+            logger.silly('test GET https://example.com (via axios)');
+            const exampleHttpsResponse = await axios.get('https://example.com');
+            if (exampleHttpsResponse.status === 200) {
+                logger.silly('SUCCESS: GET https://example.com');
+            }
 
-        logger.silly('Looking up user management service address via raw axios request');
-        const requestConfig: AxiosRequestConfig = {
-            params: { ServiceName: 'user-management', StageName: stage }
-        };
-        try {
+            logger.silly('Looking up user management service address via raw axios request');
+            const requestConfig: AxiosRequestConfig = {
+                params: { ServiceName: 'user-management', StageName: stage }
+            };
+
             const response = await axios.get(process.env.DISCOVERY_SERVICE + '/catalog/service', requestConfig);
             if (response.status === 200) {
                 logger.silly('Located user management service via axios');
             }
-        } catch (error) {
-            logger.silly(error.message);
-            logger.silly(error.stack);
-            if (error.response !== undefined) {
-                logger.silly(error.response);
-            }
         }
+
+        // Authentication & Resource lookups
+        logger.info('Looking up user management service address');
+        let endpoints = await sdk.lookupService('user-management', stage);
+        process.env.USER_MANAGEMENT_URI = endpoints[0];
+
+        logger.info('Looking up user management service address');
+        endpoints = await sdk.lookupService('data-ingestion', stage);
+        process.env.DATA_INGESTION_URI = endpoints[0];
+
+        logger.silly('authManager.signIn');
+        const cognitoSession = await authManager.signIn(process.env.ASTRA_CLOUD_USERNAME, process.env.ASTRA_CLOUD_PASSWORD);
+        logger.silly('authManager.getIamCredentials');
+        AWS.config.credentials = await authManager.getIamCredentials();
+
+        // lookup SQS queue for this tenant
+        const credentialsBearerToken: BearerTokenCredentials = {
+            idToken: cognitoSession.getIdToken().getJwtToken(),
+            type: 'BearerToken'
+        };
+        const dataIngestionApi = new DataIngestionApi(
+            process.env.DATA_INGESTION_URI,
+            region,
+            credentialsBearerToken);
+
+        logger.info('Looking up ingestion configuration info');
+        // TODO: lookup SNS topics for implementing notifications when uploads are complete
+
+        logger.info('Looking up ingestion tenant configuration info');
+        const poolListResponse = await dataIngestionApi.getTenantSettings();
+        const queueUrl = poolListResponse.data.tenantDataIngestionQueueUrl;
+        const bucketPath = poolListResponse.data.dataIngestionBucketPath;
+
+        // App
+        container.bind<Agent>(TYPES.Agent).to(Agent).inSingletonScope();
+
+        // AWS
+        container.bind<SQS>(TYPES.SQS).toConstantValue(new SQS());
+
+        // Config injection
+        container.bind<AuthManager>(TYPES.AuthManager).toConstantValue(authManager);
+        container.bind<Winston.Logger>(TYPES.Logger).toConstantValue(logger);
+        container.bind<string>(TYPES.QueueUrl).toConstantValue(queueUrl);
+        container.bind<string>(TYPES.Bucket).toConstantValue(bucketPath);
+        container.bind<IntegrationConfigFactory>(TYPES.IntegrationConfigFactory)
+            .to(IntegrationConfigFactory).inSingletonScope();
+        container.bind<IConnectionPool>(TYPES.ConnectionPool).to(OracleConnectionPoolProxy).inSingletonScope();
+
+        // Message Management
+        container.bind<MessageHandlerFactory>(TYPES.MessageHandlerFactory).to(MessageHandlerFactory).inSingletonScope();
+        container.bind<IMessageHandler>(TYPES.SendDataHandler).to(SendDataHandler);
+        container.bind<IMessageHandler>(TYPES.PreviewHandler).to(PreviewHandler);
+
+        // Messages
+        container.bind<MessageFactory>(TYPES.MessageFactory).to(MessageFactory).inSingletonScope();
+        container.bind<IMessage>(TYPES.SendDataMessage).to(SendDataMessage);
+        container.bind<IMessage>(TYPES.PreviewMessage).to(PreviewMessage);
+
+        // Data Access
+        container.bind<IDataReader>(TYPES.DataReader).to(OracleReader);
+        container.bind<IDataWriter>(TYPES.DataWriter).to(S3Writer);
+        container.bind<IDDLHelper>(TYPES.DDLHelper).to(OracleDDLHelper).whenTargetNamed(IntegrationSystemType.Oracle);
+
+        // Agent Commands
+        container.bind<ICommand>(TYPES.INGEST).to(AdHocIngestCommand);
+        container.bind<ICommand>(TYPES.PREVIEW).to(AdHocPreviewCommand);
+
+        // TODO: Revisit, is this necessary?
+        container.bind<Container>(TYPES.Container).toConstantValue(container);
+
+        return container;
+    } catch (error) {
+        logger.error('Data ingestion agent initialization error: ' + error.message);
+        // Log detail for axios errors
+        if (error.response !== undefined && error.response.data !== undefined) {
+            logger.debug('Axios response data: ' + JSON.stringify(error.response.data));
+        }
+        throw error;
     }
-
-    // Authentication & Resource lookups
-    logger.info('Looking up user management service address');
-    let endpoints = await sdk.lookupService('user-management', stage);
-    process.env.USER_MANAGEMENT_URI = endpoints[0];
-
-    logger.info('Looking up user management service address');
-    endpoints = await sdk.lookupService('data-ingestion', stage);
-    process.env.DATA_INGESTION_URI = endpoints[0];
-
-    logger.silly('authManager.signIn');
-    const cognitoSession = await authManager.signIn(process.env.ASTRA_CLOUD_USERNAME, process.env.ASTRA_CLOUD_PASSWORD);
-    logger.silly('authManager.getIamCredentials');
-    AWS.config.credentials = await authManager.getIamCredentials();
-
-    // lookup SQS queue for this tenant
-    const credentialsBearerToken: BearerTokenCredentials = {
-        idToken: cognitoSession.getIdToken().getJwtToken(),
-        type: 'BearerToken'
-    };
-    const dataIngestionApi = new DataIngestionApi(
-        process.env.DATA_INGESTION_URI,
-        region,
-        credentialsBearerToken);
-
-    logger.info('Looking up ingestion configuration info');
-    // TODO: lookup SNS topics for implementing notifications when uploads are complete
-
-    logger.info('Looking up ingestion tenant configuration info');
-    const poolListResponse = await dataIngestionApi.getTenantSettings();
-    const queueUrl = poolListResponse.data.tenantDataIngestionQueueUrl;
-    const bucketPath = poolListResponse.data.tenant_id;
-
-    // App
-    container.bind<Agent>(TYPES.Agent).to(Agent).inSingletonScope();
-
-    // AWS
-    container.bind<SQS>(TYPES.SQS).toConstantValue(new SQS());
-
-    // Config injection
-    container.bind<AuthManager>(TYPES.AuthManager).toConstantValue(authManager);
-    container.bind<Winston.Logger>(TYPES.Logger).toConstantValue(logger);
-    container.bind<string>(TYPES.QueueUrl).toConstantValue(queueUrl);
-    container.bind<string>(TYPES.Bucket).toConstantValue(bucketPath);
-    container.bind<IntegrationConfigFactory>(TYPES.IntegrationConfigFactory)
-        .to(IntegrationConfigFactory).inSingletonScope();
-    container.bind<IConnectionPool>(TYPES.ConnectionPool).to(OracleConnectionPoolProxy).inSingletonScope();
-
-    // Message Management
-    container.bind<MessageHandlerFactory>(TYPES.MessageHandlerFactory).to(MessageHandlerFactory).inSingletonScope();
-    container.bind<IMessageHandler>(TYPES.SendDataHandler).to(SendDataHandler);
-    container.bind<IMessageHandler>(TYPES.PreviewHandler).to(PreviewHandler);
-
-    // Messages
-    container.bind<MessageFactory>(TYPES.MessageFactory).to(MessageFactory).inSingletonScope();
-    container.bind<IMessage>(TYPES.SendDataMessage).to(SendDataMessage);
-    container.bind<IMessage>(TYPES.PreviewMessage).to(PreviewMessage);
-
-    // Data Access
-    container.bind<IDataReader>(TYPES.DataReader).to(OracleReader);
-    container.bind<IDataWriter>(TYPES.DataWriter).to(S3Writer);
-    container.bind<IDDLHelper>(TYPES.DDLHelper).to(OracleDDLHelper).whenTargetNamed(IntegrationSystemType.Oracle);
-
-    // Agent Commands
-    container.bind<ICommand>(TYPES.INGEST).to(AdHocIngestCommand);
-    container.bind<ICommand>(TYPES.PREVIEW).to(AdHocPreviewCommand);
-
-    // TODO: Revisit, is this necessary?
-    container.bind<Container>(TYPES.Container).toConstantValue(container);
-
-    return container;
 };
 
 export default startup;
