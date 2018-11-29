169723b76b66f81903da312ba02f1ace62c897e1
Cory Routledge <croutledge@aais.com>
2018-11-29 15:04:24 -0600

    Updated to use all_tables and owner in the get_ddl call to hopefully return proper DDL statements

--- test/unit/OracleDDLHelper.spec.ts
+++ test/unit/OracleDDLHelper.spec.ts
@@ -15,11 +15,7 @@ describe('OracleDDLHelper', () => {
             const query = helper.getDDLQuery(['table1']);
 
             const expectedQuery =
-                'SELECT 1 as "priority", \'table1\' as table_name, DBMS_METADATA.GET_DDL(\'TABLE\', \'table1\') as ddl ' +
-                'FROM dual ' +
-                'union all ' +
-                'SELECT 2 as "priority", \'table1\' as table_name, DBMS_METADATA.GET_DDL(\'INDEX\', uidx.INDEX_NAME) as ddl ' +
-                'FROM USER_INDEXES uidx where table_name = \'table1\' ';
+                `SELECT 1 as "priority", TABLE_NAME, DBMS_METADATA.GET_DDL('TABLE', TABLE_NAME, OWNER) as ddl FROM ALL_TABLES where TABLE_NAME = 'table1' `;
 
             expect(query).to.equal(expectedQuery);
         });
