842501197da0c8d62c5d8d183f56e293ca1236a0
Cory Routledge <croutledge@aais.com>
2018-12-07 13:26:53 -0600

    Adding an ingest system test to cover basic ingestion operations using Demo

--- test/system/agent.on.centos7.spec.ts
+++ test/system/agent.on.centos7.spec.ts
@@ -8,6 +8,14 @@ import SshCommandInvoker from './util/CommandInvokers/SshCommandInvoker';
 import { InstallDockerCommand } from './util/InstanceCommands/CentOS7/Commands';
 import * as DockerCommands from './util/InstanceCommands/Docker/Commands';
 import sleep from '../../source/Util/sleep';
+import * as AWS from 'aws-sdk';
+import { AuthManager, CognitoUserPoolLocatorUserManagement } from '@adastradev/user-management-sdk';
+import { BearerTokenCredentials, DiscoverySdk } from '@adastradev/serverless-discovery-sdk';
+import { DataIngestionApi } from '@adastradev/data-ingestion-sdk';
+
+import fetch from 'fetch-with-proxy';
+// tslint:disable-next-line:no-string-literal
+global['fetch'] = fetch;
 
 const expect = chai.expect;
 
@@ -18,6 +26,21 @@ describe('Data Ingestion Agent on CentOS 7', () => {
     const targetInstanceType: string = 'linux_centos7';
     let sshClient: node_ssh = null;
     let commandInvoker: IInvoker<string, string> = null;
+
+    const getLatestIngestionObject = async (s3: AWS.S3, bucketPath: string): Promise<AWS.S3.Object> => {
+        const today = new Date();
+        const ymd = { year: today.getUTCFullYear, month: today.getUTCMonth, day: today.getUTCDay };
+        const previousObjects = await s3.listObjectsV2({ Bucket: bucketPath.split('/')[0], Prefix: bucketPath.split('/')[1] + `/Demo-${ymd.year}-${ymd.month}-${ymd.day}-` }).promise();
+
+        const sortedObjects = previousObjects.Contents.sort((a, b) => {
+            if (a.LastModified < b.LastModified) { return -1; }
+            if (a.LastModified > b.LastModified) { return 1; }
+
+            return 0;
+        });
+
+        return sortedObjects[sortedObjects.length - 1];
+    };
 
     before(async () => {
         await ec2Factory.createTestInstance(targetInstanceType);
@@ -41,7 +64,7 @@ describe('Data Ingestion Agent on CentOS 7', () => {
 
     describe('when the latest docker image has been pulled', () => {
         it('the image can be started and report an initial healthy state', async () => {
-            let result = await commandInvoker.invoke(new DockerCommands.RunDataIngestionAgent());
+            let result = await commandInvoker.invoke(new DockerCommands.RunDataIngestionAgentDemo());
             await sleep(30000);
 
             result = await commandInvoker.invoke(new DockerCommands.InspectHealthStatusCommand());
@@ -52,8 +75,59 @@ describe('Data Ingestion Agent on CentOS 7', () => {
         });
 
         it('the preview command can be used to output queries to be executed in interactive mode', async () => {
-            let result = await commandInvoker.invoke(new DockerCommands.RunDataIngestionAgentWithPreview());
+            let result = await commandInvoker.invoke(new DockerCommands.RunDataIngestionAgentDemoWithPreview());
             expect(result).to.contain('SELECT * FROM ALL_TABLES');
+
+            result = await commandInvoker.invoke(new DockerCommands.StopAndRemoveContainerCommand());
+            expect(result).to.eq('diadia');
+        });
+
+        it('the ingest command can be used to ingest demo data', async () => {
+            const region = process.env.AWS_REGION;
+            const sdk: DiscoverySdk = new DiscoverySdk(process.env.DISCOVERY_SERVICE, region);
+
+            const stage = process.env.DEFAULT_STAGE || 'prod';
+            let endpoints = await sdk.lookupService('user-management', stage);
+            process.env.USER_MANAGEMENT_URI = endpoints[0];
+
+            endpoints = await sdk.lookupService('data-ingestion', stage);
+            const dataIngestionEndpoint = endpoints[0];
+
+            const poolLocator = new CognitoUserPoolLocatorUserManagement(region);
+            const authManager = new AuthManager(poolLocator, region);
+            const cognitoSession = await authManager.signIn(process.env.ASTRA_CLOUD_USERNAME, process.env.ASTRA_CLOUD_PASSWORD);
+
+            const credentialsBearerToken: BearerTokenCredentials = {
+                idToken: cognitoSession.getIdToken().getJwtToken(),
+                type: 'BearerToken'
+            };
+            const dataIngestionApi = new DataIngestionApi(
+                dataIngestionEndpoint,
+                region,
+                credentialsBearerToken);
+
+            const poolListResponse = await dataIngestionApi.getTenantSettings();
+            const bucketPath = poolListResponse.data.dataIngestionBucketPath;
+
+            const s3 = new AWS.S3();
+
+            const lastKnownObject = await getLatestIngestionObject(s3, bucketPath);
+
+            if (lastKnownObject) {
+                console.log(`Last Known Ingestion: ${lastKnownObject.Key}`);
+            }
+
+            let result = await commandInvoker.invoke(new DockerCommands.RunDataIngestionAgentDemoWithIngest());
+
+            const latestKnownObject = await getLatestIngestionObject(s3, bucketPath);
+            console.log(`Latest Known Ingestion: ${latestKnownObject.Key}`);
+
+            if (lastKnownObject) {
+                expect(latestKnownObject).to.exist;
+                expect(latestKnownObject.Key).to.not.equal(lastKnownObject.Key);
+            } else {
+                expect(latestKnownObject).to.exist;
+            }
 
             result = await commandInvoker.invoke(new DockerCommands.StopAndRemoveContainerCommand());
             expect(result).to.eq('diadia');
--- test/system/util/InstanceCommands/Docker/Commands.ts
+++ test/system/util/InstanceCommands/Docker/Commands.ts
@@ -41,7 +41,7 @@ export class StopAndRemoveContainerCommand implements ICommand<string> {
  * @class RunDataIngestionAgent
  * @implements {ICommand<string>}
  */
-export class RunDataIngestionAgent implements ICommand<string> {
+export class RunDataIngestionAgentDemo implements ICommand<string> {
     public getCommands(): Iterable<string> {
         const astraCloudUserName = process.env.ASTRA_CLOUD_USERNAME;
         const astraCloudPassword = process.env.ASTRA_CLOUD_PASSWORD;
@@ -62,13 +62,13 @@ export class RunDataIngestionAgent implements ICommand<string> {
 }
 
 /**
- * Run the ingestion agent in interactive mode to preview queries used by the agent
+ * Run the ingestion agent in interactive mode to preview Demo queries used by the agent
  *
  * @export
  * @class RunDataIngestionAgentWithPreview
  * @implements {ICommand<string>}
  */
-export class RunDataIngestionAgentWithPreview implements ICommand<string> {
+export class RunDataIngestionAgentDemoWithPreview implements ICommand<string> {
     public getCommands(): Iterable<string> {
         const astraCloudUserName = process.env.ASTRA_CLOUD_USERNAME;
         const astraCloudPassword = process.env.ASTRA_CLOUD_PASSWORD;
@@ -84,6 +84,33 @@ export class RunDataIngestionAgentWithPreview implements ICommand<string> {
             `-e INTEGRATION_TYPE=Demo`,
             `adastradev/data-ingestion-agent:${normalizedDockerTag}`,
             'preview'
+        ].join(' ')];
+    }
+}
+
+/**
+ * Run the ingestion agent in interactive mode to ingest using the Demo integration type
+ *
+ * @export
+ * @class RunDataIngestionAgentDemoWithIngest
+ * @implements {ICommand<string>}
+ */
+export class RunDataIngestionAgentDemoWithIngest implements ICommand<string> {
+    public getCommands(): Iterable<string> {
+        const astraCloudUserName = process.env.ASTRA_CLOUD_USERNAME;
+        const astraCloudPassword = process.env.ASTRA_CLOUD_PASSWORD;
+        const normalizedDockerTag = process.env.NORMALIZED_DOCKER_TAG;
+
+        return [[
+            'sudo docker run',
+            '--name dia',
+            '-i',
+            `-e ASTRA_CLOUD_USERNAME=${astraCloudUserName}`,
+            `-e ASTRA_CLOUD_PASSWORD=${astraCloudPassword}`,
+            `-e PROCESS_MAX_MEMORY_SIZE_MB=512`,
+            `-e INTEGRATION_TYPE=Demo`,
+            `adastradev/data-ingestion-agent:${normalizedDockerTag}`,
+            'ingest'
         ].join(' ')];
     }
 }
