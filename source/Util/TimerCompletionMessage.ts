7a7dbf7e764064aa6365b760fbd4496ae8b66dec
Cory Routledge <croutledge@aais.com>
2018-12-10 17:29:22 -0600

    Cleanup

--- source/Util/TimerCompletionMessage.ts
+++ source/Util/TimerCompletionMessage.ts
@@ -1,10 +1,8 @@
-import * as moment from 'moment';
-
 export default class TimerCompletionMessage {
     constructor(public readonly message: string, public readonly token: string, public readonly startDateTime: number) {
     }
 
     public generate(durationDescription: string): string {
-        return this.message.replace(`${this.token}`, durationDescription);
+        return this.message.replace(this.token, durationDescription);
     }
 }
