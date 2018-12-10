02bab9b70d22c23033c28bf5e1b8a84bb919bbfe
Matt Cook <mcook@aais.com>
2018-12-10 08:45:22 -0600

    Set up DataAccessDoc class

--- source/DataAccess/DADGenerator.ts
+++ source/DataAccess/DADGenerator.ts
@@ -3,8 +3,33 @@
 import IntegrationConfigFactory from '../IntegrationConfigFactory';
 import { IIntegrationConfig, IntegrationType } from '../IIntegrationConfig';
 import { writeFile } from 'fs';
+import IDataAccessDoc from './IDataAccessDoc';
 
 let integrationTypes = [
     IntegrationType.Banner,
     IntegrationType.DegreeWorks
 ]
+
+class DataAccessDoc implements IDataAccessDoc {
+    
+    integrationType;
+    queries;
+    header;
+    footer;
+
+    constructor(integrationType: string) {
+        this.integrationType = integrationType;
+        this.header.push('HEADER');
+        this.footer.push('FOOTER');
+        
+        let icf = new IntegrationConfigFactory();
+        let cfg: IIntegrationConfig = icf.create(this.integrationType);
+
+        this.queries = cfg.queries;
+    }
+
+    getQueryInfo() { return null };
+    createTable() { return [""] }
+    create() { };
+    
+}
