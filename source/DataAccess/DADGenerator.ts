35ebaa16f2124d117bacb9326b1201dba64ca9bd
Matt Cook <mcook@aais.com>
2018-12-10 15:21:05 -0600

    Fix linting errors and refactor to single pre-commits file

--- source/DataAccess/DADGenerator.ts
+++ source/DataAccess/DADGenerator.ts
@@ -1,67 +1,56 @@
-'use strict'
+'use strict';
 
 import IntegrationConfigFactory from '../IntegrationConfigFactory';
 import { IIntegrationConfig, IntegrationType } from '../IIntegrationConfig';
-import { writeFile, mkdir } from 'fs';
-import IDataAccessDoc from './IDataAccessDoc';
+import { mkdir, writeFile } from 'fs';
 
-let integrationEnumKeys = Object.keys(IntegrationType).slice(0, -2)
-let integrationTypes = integrationEnumKeys.map(key => {
-    return IntegrationType[key];
-})
+export default class DataAccessDoc {
 
-class DataAccessDoc implements IDataAccessDoc {
-    
-    integrationType;
-    queries;
-    header: string[] = [];
-    footer: string[] = [];
+    private _integrationType;
+    private _queries;
+    private _header: string[] = [];
+    private _footer: string[] = [];
 
     constructor(integrationType: string) {
-        this.integrationType = integrationType;
-        this.header.push(`# Data Access Requirements: ${this.integrationType}`);
-        this.header.push('The following tables and fields are required for the DIA with integrationConfig: ' + this.integrationType);
-        this.footer.push('');
-        
-        let icf = new IntegrationConfigFactory();
-        let cfg: IIntegrationConfig = icf.create(this.integrationType);
+        this._integrationType = integrationType;
+        this._header.push(`# Data Access Requirements: ${this._integrationType}`);
+        this._header.push('The following tables and fields are required for the DIA with integrationConfig: ' + this._integrationType);
+        this._footer.push('');
 
-        this.queries = cfg.queries;
+        const icf = new IntegrationConfigFactory();
+        const cfg: IIntegrationConfig = icf.create(this._integrationType);
+
+        this._queries = cfg.queries;
     }
 
-    getQueryInfo(): any {
-        return this.queries.map(query => {
-            let tableName = query.name;
-            let splitQuery = query.query.replace(/,/g , '').toUpperCase().split(' ');
-            let fields = splitQuery.slice(1, splitQuery.indexOf('FROM'));
+    public create(): void {
+        const path = './docs/DataAccess/' + this._integrationType + '.md';
+        const data = this._header.join('\n') + '\n\n' + this._createTable().join('\n') + '\n\n' + this._footer.join('\n');
+        writeFile(path, data, (err) => {
+            if (err) {
+                console.log(err);
+            }
+        });
+    }
+
+    private _getQueryInfo(): any {
+        return this._queries.map((query) => {
+            const tableName = query.name;
+            const splitQuery = query.query.replace(/,/g , '').toUpperCase().split(' ');
+            const fields = splitQuery.slice(1, splitQuery.indexOf('FROM'));
             return { tableName, fields };
         });
     }
 
-    createTable(): string[] {
-        let tableHead = [ '| Tables | Fields |' , '| ------ | ------ |' ];
-        let queryInfo = this.getQueryInfo();
-        let tableBody = [];
-        queryInfo.forEach(query => {
-            tableBody.push(`| ${ query.tableName } | ${ query.fields.join(', ') } |`)
+    private _createTable(): string[] {
+        const tableHead = [ '| Tables | Fields |' , '| ------ | ------ |' ];
+        const queryInfo = this._getQueryInfo();
+        const tableBody = [];
+        queryInfo.forEach((query) => {
+            tableBody.push(`| ${ query.tableName } | ${ query.fields.join(', ') } |`);
         });
-        let table = tableHead.concat(tableBody);
+        const table = tableHead.concat(tableBody);
         return table;
     }
 
-    create(): void {
-        let path = './docs/DataAccess/' + this.integrationType + '.md';
-        let data = this.header.join('\n') + '\n\n' + this.createTable().join('\n') + '\n\n' + this.footer.join('\n');
-        writeFile(path, data, (err) => {
-            if (err) { 
-                console.log(err);
-            };
-        });
-    }
-
 }
-
-integrationTypes.forEach(type => {
-    let DAD = new DataAccessDoc(type);
-    DAD.create();
-});
---
+++ source/Util/pre-commits.ts
@@ -0,0 +1,14 @@
+import { IntegrationType } from '../IIntegrationConfig';
+import DataAccessDoc from '../DataAccess/DADGenerator';
+
+const integrationEnumKeys = Object.keys(IntegrationType).slice(0, -2);
+const integrationTypes = integrationEnumKeys.map((key) => {
+    return IntegrationType[key];
+});
+
+// Create DataAccessDocs
+
+integrationTypes.forEach((type) => {
+    const DAD = new DataAccessDoc(type);
+    DAD.create();
+});
