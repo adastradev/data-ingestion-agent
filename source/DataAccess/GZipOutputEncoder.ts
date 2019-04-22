db8e81fe145c1911544acbf819b7b92d2a33f64a
Cory Routledge <croutledge@aais.com>
2019-04-22 10:41:44 -0500

    Forgot to add injectable annotation to the gzip encoder

--- source/DataAccess/GZipOutputEncoder.ts
+++ source/DataAccess/GZipOutputEncoder.ts
@@ -1,7 +1,8 @@
 import IOutputEncoder, { IEncodeResult } from './IOutputEncoder';
 import { Readable } from 'stream';
 import * as zlib from 'zlib';
-
+import { injectable } from 'inversify';
+@injectable()
 export default class GZipOutputEncoder implements IOutputEncoder {
     public encode(inputStream: Readable): IEncodeResult {
         return { outputStream: inputStream.pipe(zlib.createGzip()), extension: 'gz' };
